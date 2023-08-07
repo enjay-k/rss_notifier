04d0805aa6c637491b3ac1d36e338693c0455165
enjay-k <kmn.njabulo@gmail.com>
2023-08-07 21:13:33 +0200

    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to RSS notifier where all the information you need on ESPN FC will be provided</Text>
       <StatusBar style="auto" />
     </View>
   );
